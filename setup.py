4cf5ac2f3c03f5db78c341a0a5750b061c35f77a
FELD Boris <lothiraldan@gmail.com>
2012-09-29 14:44:43 +0200

    Read README file for long-description

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
+import os
 from setuptools import setup
+
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(name='picomongo',
       version='0.6',
@@ -13,5 +20,6 @@ setup(name='picomongo',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python',
         'Topic :: Database',
-      ]
+      ],
+      long_description=read('README.rst')
 )
